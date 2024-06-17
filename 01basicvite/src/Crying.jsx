18ef1f1b1f74a3c7942a168c8f9f507fcfa69be4
popcorn150 <hungrymanchannel@gmail.com>
2024-06-17 21:38:25 +0100

    deleted old files

--- 01basicvite/src/Crying.jsx
+++
@@ -1,6 +0,0 @@
-function Crying() {
-    return(
-        <h6>I cried so hard</h6>
-    )
-}
-export default Crying
--- 01basicvite/src/Laugh.jsx
+++
@@ -1,7 +0,0 @@
-function Laughing(){
-    return (
-        <h3>Hahaha I laughed so hard</h3>
-    )
-}
-
-export default Laughing
