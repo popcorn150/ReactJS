38b7bb30b2d462fc0a3f086a051f2a2c047ba043
popcorn150 <hungrymanchannel@gmail.com>
2024-07-31 19:18:57 +0100

    react lesson & changes added

--- ghost-blog/src/App.js
+++ ghost-blog/src/App.js
@@ -1,30 +1,74 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Create from './Create';
-import BlogDetails from './BlogDetails';
+// function Item({ name, isPacked, importance }) {
+//   return (
+//     <li className="item">
+//       {isPacked ? name + ' ✔' : name + ' ❌'}
+//       {importance > 0 && ' '}
+//       {importance > 0 && <i>
+//         (Importance: {importance})</i>
+//       }
+//     </li>
+//   );
+// }
 
-function App() {
+// export default function PackingList() {
+//   return (
+//     <section>
+//       <h1>Sally's Ride Packing List</h1>
+//       <ul>
+//         <Item
+//           isPacked={true}
+//           name="Space Unit"
+//           importance={9}
+//         />
+//         <Item
+//           isPacked={true}
+//           name="Helmet with a golden leaf"
+//           importance={0}
+//         />
+//         <Item
+//           isPacked={false}
+//           name="Photo of Tam"
+//           importance={6}
+//         />
+//       </ul>
+//     </section>
+//   );
+// }
+
+function Drink({ name }) {
+
+  let part, caffeine, age;
+  if (name === 'tea') {
+    part = 'leaf';
+    caffeine = '15–70 mg/cup';
+    age = '4,000+ years';
+  } else {
+    part = 'bean';
+    caffeine = '80–185 mg/cup';
+    age = '1,000+ years'
+  }
+
+
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/blogs/:id">
-              <BlogDetails />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{part}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{caffeine}</dd>
+        <dt>Age</dt>
+        <dd>{age}</dd>
+      </dl>
+    </section>
   );
 }
 
-export default App;
+export default function DrinkList() {
+  return (
+    <div>
+      <Drink name="tea" />
+      <Drink name="coffee" />
+    </div>
+  );
+}
--- ghost-mobile/scr/views/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-    return (
-        <div className="page404">
-            <h1>404 Page Not Found</h1>
-        </div>
-    )
-}
